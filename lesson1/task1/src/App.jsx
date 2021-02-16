5e6555ed6d47454563072ea8b1860c73d0f7591a
Volodymyr Meduna <liodik89@gmail.com>
2021-02-16 14:16:03 +0200

    first commit

---
+++ lesson1/task1/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ThemedButton from './ThemedButton';
+import { themes, ThemeContext } from './themes-context';
+class App extends Component {
+  state = {
+    theme: themes.dark,
+  };
+  toggleTheme = () => {
+    const newTheme = this.state.theme === themes.dark ? themes.light : themes.light;
+    this.setState({
+      theme: newTheme,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <ThemedButton onClick={this.toggleTheme}>Dynamic Theme</ThemedButton>
+        </ThemeContext.Provider>
+        <ThemedButton onClick={this.toggleTheme}>Default Theme</ThemedButton>
+      </div>
+    );
+  }
+}
+
+export default App;
