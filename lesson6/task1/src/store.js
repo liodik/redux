96931d6e9ed9f5fc9a95137b44e497930b46d2d9
Volodymyr Meduna <liodik89@gmail.com>
2021-02-22 09:24:50 +0200

    commit

---
+++ lesson6/task1/src/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import counterReducer from './counter.reducer';
+
+const logger = srore => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  const result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(counterReducer, composeEnhancers(applyMiddleware(logger)));
+
+export default store;
